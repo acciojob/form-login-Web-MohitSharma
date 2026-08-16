944309dd78197c06a0a5b86c78aaaf9b5f471cc0
Mohit Sharma <skmohit9955@gmail.com>
2026-08-16 17:24:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	let fName = document.querySelector(input)[0].value.trim();
+	let lName = document.querySelector(input)[1].value.trim();
 
+	alert(`${fName} ${lName}`)
+	
 }
